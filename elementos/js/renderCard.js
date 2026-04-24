7f973a368f927ec091703a52d89195d801ac21e4
ezequielmaureira <94615363+ezequielmaureira@users.noreply.github.com>
2026-04-24 09:26:56 -0300

    Update renderCard.js

--- elementos/js/renderCard.js
+++ elementos/js/renderCard.js
@@ -1,1 +1,13 @@
+async function crearCard(evento) {
+  const res = await fetch("elementos/card_evento.html");
+  const html = await res.text();
 
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = html;
+
+  wrapper.querySelector(".title").textContent = evento.name;
+  wrapper.querySelector(".date").textContent = "📅 " + evento.date;
+  wrapper.querySelector(".price").textContent = "💰 $" + evento.price;
+
+  return wrapper.firstElementChild;
+}
