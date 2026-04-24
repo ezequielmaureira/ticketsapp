80b0838737e057045fa7e98ff6ffffc7253f67c4
ezequielmaureira <94615363+ezequielmaureira@users.noreply.github.com>
2026-04-24 11:02:27 -0300

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,16 @@
+async function crearCard(evento) {
+  const div = document.createElement("div");
 
+  div.style.border = "1px solid #ccc";
+  div.style.padding = "10px";
+  div.style.margin = "10px";
+  div.style.borderRadius = "8px";
+
+  div.innerHTML = `
+    <h3>${evento.name}</h3>
+    <p>📅 ${evento.date}</p>
+    <p>💰 $${evento.price}</p>
+  `;
+
+  return div;
+}
